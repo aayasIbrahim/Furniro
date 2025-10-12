a5276115517ad3673929ab2e7ade2a6d550e1cb9
Ayas Ibrahim <aayasabraham@gmail.com>
2025-10-12 17:31:44 +0600

    Product Schema ready

---
+++ src/models/Product.ts
@@ -0,0 +1,51 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IProduct extends Document {
+  name: string;
+  description: string;
+  price: number;
+  oldPrice?: number;
+  imageUrl: string;
+  badge?: string;
+  isFeatured: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const ProductSchema: Schema<IProduct> = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    oldPrice: {
+      type: Number,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+    },
+    badge: {
+      type: String,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product =
+  mongoose.models.Product || mongoose.model<IProduct>("Product", ProductSchema);
+
+export default Product;
